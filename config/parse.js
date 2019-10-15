ec1de97d0a36163f4c285a5c445aaca8daa6fb25
Andrew Clark <andrewclark.ku@gmail.com>
2019-10-15 17:17:48 -0500

    parsed data now displays on dashboard page

--- config/parse.js
+++ config/parse.js
@@ -2,24 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Parser = require('rss-parser');
 
-
+let parser = new Parser();
 
 module.exports = {
-    parseLink: async function(value){
-
-        let parser = new Parser();
-
-        (async () => {
- 
-            let feed = await parser.parseURL(value);
-            console.log(feed.title);
-           
-            feed.items.forEach(item => {
-              console.log(item.title + ':' + item.link)
-            });
-           
-          })();
-
-
-    }
+  parseLink: async function(value){
+      let feed = await parser.parseURL(value);
+      return feed;
+  }
 };
--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,17 @@ const { parseLink } = require('../config/parse');
 router.get('/', (req, res) => res.render('welcome'));
 
 // Dashboard page
-router.get('/dashboard', ensureAuthenticated, (req, res) => {
-
-    let temp = req.user.urls;
-    let temp_array = [];
-    temp.forEach(async function(o) {
-        let temp_url = (o.link); 
-        await parseLink(temp_url);
+router.get('/dashboard', ensureAuthenticated, async (req, res) => {
+    let feedRequests = req.user.urls.map(function(o) {
+        return parseLink(o.link);
     });
 
-    res.render('dashboard', {
-        name: req.user.name,
-        urls: req.user.urls
+    Promise.all(feedRequests).then((data) => {
+        res.render('dashboard', {
+            name: req.user.name,
+            urls: req.user.urls,
+            feeds: data,
+        })
     })
 });
 
