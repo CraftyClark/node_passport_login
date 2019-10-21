60de95ed70c4c0b2e49b34e1a169395e8700a6d7
Andrew Clark <andrewclark.ku@gmail.com>
2019-10-21 09:33:33 -0500

    trying to fix port fail issue when deployed to heroku

--- app.js
+++ app.js
@@ -52,6 +52,8 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server Started on port ${PORT}`));
+app.listen(PORT, function() {
+  console.log(`Server Started on port ${PORT}`);
+});
